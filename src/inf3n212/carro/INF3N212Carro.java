a347e7e8dd7742752ea376f6d446d12b5bf55825
Thiago Silva Nunes <182120021@senacinformatica.br>
2023-03-29 21:53:12 -0300

    Criacao metodo menu principal e sub menu

--- src/inf3n212/carro/INF3N212Carro.java
+++ src/inf3n212/carro/INF3N212Carro.java
@@ -6,28 +6,68 @@ package inf3n212.carro;
 
 import controller.CCarro;
 import controller.CPessoa;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  *
  * @author jbferraz
  */
 public class INF3N212Carro {
-    
+
+    static Scanner leia = new Scanner(System.in);
     public static CPessoa cadPessoa = new CPessoa();
     public static CCarro cadCarro = new CCarro();
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         cadPessoa.mockPessoas();
         cadCarro.mockCarros();
-        
+
         System.out.println(cadPessoa.getPessoas());
         System.out.println("----");
         System.out.println(cadCarro.getCarros());
-        
+
+    }//fim método manin
+
+    public static int leiaNumInt() {
+        Scanner leiaNum = new Scanner(System.in);
+        try {
+            return leiaNum.nextInt();
+        } catch (InputMismatchException i) {
+            System.out.println("Erro: " + i.getMessage() + "\nTente novamente!");
+            leiaNumInt();
+
+        }
+        return 99;
     }
-    
-}
+
+    public static void menu() {
+        System.out.println("-- Menu Principal -- ");
+        System.out.println("1 - Ger. Pessoa");
+        System.out.println("2 - Ger. Carro");
+        System.out.println("0 - Sair");
+        System.out.println("Digite aqui");
+    }
+
+    public static void subMenu(int opM) {
+        String subM = null;
+        if (opM == 1) {
+            subM = "Pessoa";
+        }
+        if (opM == 2) {
+            subM = "carro";
+        }
+        System.out.println("--Ger." + subM);
+        System.out.println("1 - Cadastrar" + subM);
+        System.out.println("2 - Editar" + subM);
+        System.out.println("3 - Listar" + subM + "s");
+        System.out.println("4 - Deletar" + subM);
+        System.out.println("0 - voltar");
+        System.out.print("Digite aqui:");
+    }
+}//fim classe
